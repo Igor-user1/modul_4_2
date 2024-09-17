8290540f9b9f87a3cb6d6d690110a735bb5c0a01
Igor-user1 <xadeev.igor@icloud.com>
2024-09-17 12:31:13 +0300

    Update modul_4_2.py

--- modul_4_2.py
+++ modul_4_2.py
@@ -1,9 +1,9 @@
 def test_function():
     def inner_function():
-        print('Я в области видимости функции test_function')
+        return print('Я в области видимости функции test_function')
     inner_function()
     return
 
 
-print(test_function())
-print(inner_function())
+test_function()
+inner_function()
